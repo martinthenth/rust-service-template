f147d2dc4f18e62881a295b799c6b665ded1908d
Martin Nijboer <mrnijboer@gmail.com>
2025-05-09 13:06:49 +0200

    Add database migrations

---
+++ migrations/20250509104718_create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID NOT NULL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    banned_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
